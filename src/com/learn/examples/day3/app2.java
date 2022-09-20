1afac3db49285662106bde8862d1306c3ebaf2ba
njain51 <official.nitinjain@gmail.com>
2022-09-20 05:51:38 -0400

    assignments day3

--- src/com/learn/examples/day3/app2.java
+++ src/com/learn/examples/day3/app2.java
@@ -1,49 +1,54 @@
 package com.learn.examples.day3;
 
 import java.util.Scanner;
+
 /**
- What this program snippet doing?
- Given person age, Write a function that will print if the person is kid , teen or adult.
-
- teen if age is between 13 and 19 inclusive.
- kid if less than 13.
- adult if more than 19.
-
+ * What this program snippet doing?
+ * Given person age, Write a function that will print if the person is kid , teen or adult.
+ * <p>
+ * teen if age is between 13 and 19 inclusive.
+ * kid if less than 13.
+ * adult if more than 19.
  **/
 public class app2 {
 
-    public String getStatus(String age){
-          int age1 = Integer.getInteger(age);
-          if (age1 >= 13) {
+    int ageOfPerson;
 
-              return "Person is a kid" ;
-          }
-          else if (age1 > 13 && age1 <= 19) {
+    public String getStatus(int age) {
 
-              return "Person is a teen";
-          }
-          else if(age1 > 19){
+        //System.out.println("age passed is " + age);
 
-              return "Person is a adult" ;
-          }
-          else {
-              return "Not a valid age";
-          }
+        if (age <= 13) {
 
+            return "kid";
 
+        }
+        else if (age > 13 && age <= 19) {
+
+            return "teen";
+        }
+        else if (age > 19) {
+
+            return "adult";
+        }
+
+        else  {
+
+            return "Null";
+        }
 
     }
 
-
     public static void main(String[] args) {
+
         Scanner obj1 = new Scanner(System.in); // Create Scanner object
-        System.out.println("Enter age of person " );
-        String age = obj1.nextLine();
-        System.out.println("Person age is "+age);
+        System.out.println("Enter age of person ");
+        String age = obj1.next();
+        int age1 = Integer.parseInt(age);
+        System.out.println("Person age is " + age1);
 
         app2 obj2 = new app2();
-        System.out.println((obj2.getStatus(age)));
-
+        System.out.println("Person is a " + obj2.getStatus(age1));
 
 
     }
